58f726239e6876356575d422b9581f1681bf94c9
nattawutroj <nattawutroj@gmail.com>
2022-10-17 01:03:59 +0700

    no message

--- tempCodeRunnerFile.cpp
+++
@@ -1,8 +0,0 @@
-    else if (command == "r" || command == "R" || command == ".reg")
-    {
-        Reg_member();
-    }
-    else if (command == "s" || command == "S" || command == ".member")
-    {
-        Displays();
-    }
