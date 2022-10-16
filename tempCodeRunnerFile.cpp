7fbadba3526e9bf99b13ddd258622686b6117c57
nattawutroj <nattawutroj@gmail.com>
2022-10-17 01:04:31 +0700

    Revert "no message"
    This reverts commit 58f726239e6876356575d422b9581f1681bf94c9.

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,8 @@
+    else if (command == "r" || command == "R" || command == ".reg")
+    {
+        Reg_member();
+    }
+    else if (command == "s" || command == "S" || command == ".member")
+    {
+        Displays();
+    }
